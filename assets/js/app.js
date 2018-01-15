86815da064158aef4ba9445ac706149b53421ede
Anthony Kim <akim2214@gmail.com>
2018-01-14 22:51:40 -0800

    functioning javascript file attempted addition of google authentication attempted addition of delete/firebase child removal

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,6 +10,25 @@ var config = {
 
 firebase.initializeApp(config);
 
+// var provider = new firebase.auth.GoogleAuthProvider();
+
+// firebase.auth().signInWithPopup(provider).then(function(result) {
+//     // This gives you a Google Access Token. You can use it to access the Google API.
+//     var token = result.credential.accessToken;
+//     // The signed-in user info.
+//     var user = result.user;
+//     // ...
+//   }).catch(function(error) {
+//     // Handle Errors here.
+//     var errorCode = error.code;
+//     var errorMessage = error.message;
+//     // The email of the user's account used.
+//     var email = error.email;
+//     // The firebase.auth.AuthCredential type that was used.
+//     var credential = error.credential;
+//     // ...
+//   });
+
 var database = firebase.database();
 
 var name = "";
@@ -21,25 +40,29 @@ var frequency;
 $("#submit").on("click", function () {
     // Don't refresh the page!
     event.preventDefault();
+    if ($("#name-input" || "#destination-input" || "#time-input" || "#frequency-input").val().trim() == "") {
+        alert("Please complete the form");
+    }
+    else {
+        name = $("#name-input").val().trim();
+        destination = $("#destination-input").val().trim();
+        firstTime = $("#time-input").val().trim();
+        frequency = $("#frequency-input").val().trim();
 
-    name = $("#name-input").val().trim();
-    destination = $("#destination-input").val().trim();
-    firstTime = $("#time-input").val().trim();
-    frequency = $("#frequency-input").val().trim();
+        var newTime = {
+            name: name,
+            destination: destination,
+            firstTime: firstTime,
+            frequency: frequency
+        };
 
-    var newTime = {
-        name: name,
-        destination: destination,
-        firstTime: firstTime,
-        frequency: frequency
-    };
+        database.ref().push(newTime);
 
-    database.ref().push(newTime);
-
-    $("#name-input").val("");
-    $("#destination-input").val("");
-    $("#time-input").val("");
-    $("#frequency-input").val("");
+        $("#name-input").val("");
+        $("#destination-input").val("");
+        $("#time-input").val("");
+        $("#frequency-input").val("");
+    }
 });
 
 database.ref().on("child_added", function (childSnapshot, prevChildKey) {
@@ -63,7 +86,9 @@ database.ref().on("child_added", function (childSnapshot, prevChildKey) {
 
     $("#train-schedule > tbody").append(
         "<tr><td>" + name + "</td><td>" + destination + "</td><td>" +
-        frequency + "</td><td>" + nextArrival + "</td><td>" + untilNextTime + "</td></tr>");
+        frequency + "</td><td>" + nextArrival + "</td><td>" + untilNextTime + 
+        // "</td><td>" + "<button type='button' class='btn btn-danger'>Remove</button>" + 
+        "</td></tr>");
     $("#currently").append(currentTimeDisplay);
 });
 
